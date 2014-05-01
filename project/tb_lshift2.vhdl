21e24ae8dfb39079bd0a44f4ba7874d0bb2b3d54
Robert Higgins <rmhkd2@mst.edu>
2014-05-01 01:14:40 -0500

    Templated TBs

--- project/tb_lshift2.vhdl
+++ project/tb_lshift2.vhdl
@@ -1,1 +1,30 @@
 library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use lib_mips32.all;
+
+entity FUCK is
+    port ( ); 
+end entity FUCK;
+
+architecture verify of FUCK is
+    signal 
+begin
+    duv : entity work.SHIT
+        port map ( 
+            clk => clk,
+            PERTS
+        );
+
+    test : process
+    begin
+ 
+    end process test;
+
+    clkgen : process
+    begin
+        clk <= not clk;
+        wait for 1 ns;
+    end process clkgen;
+
+end architecture verify;
--- project/tb_template.vhdl
+++ project/tb_template.vhdl
@@ -1,17 +1,30 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use lib_mips32.all;
 
-entity  is
+entity FUCK is
     port ( ); 
-end entity ;
+end entity FUCK;
 
-architecture verify of  is
+architecture verify of FUCK is
     signal 
 begin
-    duv : entity work.
-        port map ( );
+    duv : entity work.SHIT
+        port map ( 
+            clk => clk,
+            PERTS
+        );
 
-    test : process is
+    test : process
     begin
+ 
+    end process test;
+
+    clkgen : process
+    begin
+        clk <= not clk;
+        wait for 1 ns;
+    end process clkgen;
 
 end architecture verify;
